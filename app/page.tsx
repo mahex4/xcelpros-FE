1165881ab28141cd01713fc2bb5740103d992d88
Mahi <mahex4@gmail.com>
2025-06-16 15:56:40 +0530

    Homepage styles

--- app/page.tsx
+++ app/page.tsx
@@ -17,10 +17,10 @@ export default async function Home() {
   return (
     <div className="w-full font-[family-name:var(--font-geist-sans)] px-4 md:px-12">
       <main className="flex flex-col gap-[32px] w-full ">
-        <section className="bg-gradient-to-br w-full md:px-4 h-screen flex flex-col md:flex-row items-center justify-between md:justify-between">
-          <div id="left-holder" className=" flex flex-col gap-6">
+        <section className="bg-gradient-to-br w-full md:px-4 h-screen flex flex-col gap-4 md:flex-row items-center justify-between md:justify-between">
+          <div id="left-holder" className=" flex flex-col gap-6 py-24">
             <div className=" text-left">
-              <h1 className="text-5xl font-bold text-gray-900 mb-4">
+              <h1 className=" md:3xl lg:text-4xl font-bold text-gray-900 mb-4">
                 Track Your Nutrition <span className="text-slate-900">Smarter</span>
               </h1>
               <p className="text-xl text-gray-600 max-w-2xl mx-auto mb-8">
